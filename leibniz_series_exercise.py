d82873d63c3d629f5ea692e4b32a44a489077725
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-12 00:29:12 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,7 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+    x = 0
+    for i in range(0, n_terms):
+        n= (-1)** i/(2*i+1)
+        x= x+n
+    y = 4*x
+    return y
