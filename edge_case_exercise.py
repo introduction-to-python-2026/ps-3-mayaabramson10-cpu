3f0c3b4ce682de753097e2b9433096b1b1178874
mayaabramson10-cpu <maya.abramson10@gmail.com>
2025-11-11 19:58:27 +0200

    Update edge_case_exercise.py

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,8 +1,8 @@
-def move(my_list, direction):
+def move(my_list, direction= None):
     # Finds the index of the one in the list
     index_of_one = my_list.index(1)
 
-    if index_of_one == (len(my_list) - 1) or index_of_one == 0:
+    if index_of_one == len(my_list) - 1 or index_of_one == 0:
       
         return my_list
     
